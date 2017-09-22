1cb0a6826f58f6a60311ad3c27fe4030bb2cef07
Joshua <jmg1138@gmail.com>
2017-09-22 13:20:19 -0700

    Basic start to the login screen.

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require(`electron`)
+const path = require(`path`)
+const url = require(`url`)
+const pug = require(`electron-pug`)({pretty: true})
 
-let mainWindow
+let win
 
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
+app.on(`window-all-closed`, () => app.quit())
+
+app.on(`ready`, () => {
+  win = new BrowserWindow({
       height: 600,
       width: 800,
-      autoHideMenuBar: true
+      autoHideMenuBar: true,
+      icon: `./public/images/favicon.ico`
   })
-
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, `./views/index.pug`),
+    protocol: `file:`,
+    slashes: true
+  }))
+  win.focus()
 })
+
+
+ipcMain.on('loginFormSubmit', (event, email, password) => {
+    console.log(`${email} ${password}`)
+});
